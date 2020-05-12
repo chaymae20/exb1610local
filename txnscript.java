28b9c1a39c66212867fb5f841e1653f621e081e0
chaymae.sosse-alaoui <63346446+chaymae20@users.noreply.github.com>
2020-05-12 22:34:54 +0000

    Update txnscript.java

--- txnscript.java
+++ txnscript.java
@@ -317,14 +317,14 @@ public class txnscript
     {
         String result = "" ;
 		
-		String sql = "UPDATE Villes SET nom = ?, code_postal = ? WHERE id = ?" ;
+		String sql = "UPDATE Villes SET code_postal = ?  WHERE nom = ?" ;
 
 		try
 		{
 				PreparedStatement pstmt = cnx.prepareStatement(sql) ;
-				pstmt.setString(1, nom);
-				pstmt.setDouble(2, codePostal);
-				pstmt.setInt(3, id);
+				pstmt.setDouble(1, codePostal);
+				pstmt.setString(2, nom);
+				//pstmt.setInt(3, id);
 				pstmt.executeUpdate();
 		}
 		catch (SQLException e)
